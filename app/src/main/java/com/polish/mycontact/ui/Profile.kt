5f2097470b9aefa1ab93e396e0c6ac14e30b46a5
Osehi <osehiaseehilen@gmail.com>
2020-01-29 05:39:27 +0100

    'delete-function-added'

--- app/src/main/java/com/polish/mycontact/ui/Profile.kt
+++ app/src/main/java/com/polish/mycontact/ui/Profile.kt
@@ -4,19 +4,23 @@ package com.polish.mycontact.ui
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 
 import com.polish.mycontact.R
 import com.polish.mycontact.databinding.FragmentProfileBinding
 import com.polish.mycontact.model.Contact
+import com.polish.mycontact.viewModel.ContactViewModel
 
 /**
  * A simple [Fragment] subclass.
  */
 class Profile : Fragment() {
 
+    var contactViewModel:ContactViewModel? = null
     var contact:Contact? = null
 
    val args:ProfileArgs by navArgs()
@@ -29,6 +33,7 @@ class Profile : Fragment() {
 //        return inflater.inflate(R.layout.fragment_profile, container, false)
 
         val binding = FragmentProfileBinding.inflate(inflater, container, false)
+         contactViewModel = ViewModelProvider(this).get(ContactViewModel::class.java)
 
 
         contact = args.contact
@@ -57,6 +62,24 @@ class Profile : Fragment() {
                 ProfileDirections.actionProfileToEdit3(contact!!)
             findNavController().navigate(action)
             Toast.makeText(context, "Edit Contact", Toast.LENGTH_SHORT).show()
+        }
+        if (id == R.id.delete){
+            val mAlertDialog = AlertDialog.Builder(context!!)
+            mAlertDialog.setTitle("Delete Contact")
+            mAlertDialog.setMessage("Delete contact permanently...")
+            mAlertDialog.setIcon(R.mipmap.ic_launcher)
+            // add a positive button to alert dialogue
+            mAlertDialog.setPositiveButton("Delete"){
+                dialog,id ->
+                contactViewModel?.delete(contact!!)
+                Toast.makeText(context, "Contact deleted", Toast.LENGTH_SHORT).show()
+                this.findNavController().navigateUp()
+            }
+            // add a negative button to alert dialogue
+            mAlertDialog.setNegativeButton("Cancel"){dialog, id ->
+                dialog.dismiss()
+            }
+            mAlertDialog.show()
         }
         return super.onOptionsItemSelected(item)
     }
